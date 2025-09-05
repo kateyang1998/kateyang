68422d5bc3fc443ab70ba4ba4dd89d38a7f64acc
Kate Yang <150059882+kateyang1998@users.noreply.github.com>
2025-09-05 14:23:18 -0400

    add: added portfolio files and resources

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const typed = new Typed('.typed-text', {
+        strings: ['I am a UX Designer', 'I am a Web / Software Developer', 'Explore More About Me'],
+        typeSpeed: 100
+    });
+});
